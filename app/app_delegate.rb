485312b0cf18ba03f1705764c37f81ea0fda9fc8
yellowstar <845059331@qq.com>
2014-05-29 22:17:03 +0800

    Hello Ruby Motion!

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,8 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    alert = UIAlertView.new
+    alert.message = "Hello Ruby Motion!"
+    alert.show
     true
   end
 end
