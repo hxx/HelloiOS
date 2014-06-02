884cddf58ff0cc30e149be50c8b152ad3d715746
yellowstar <845059331@qq.com>
2014-06-02 19:52:07 +0800

    add a label in TapController

--- app/controllers/TapController.rb
+++ app/controllers/TapController.rb
@@ -4,5 +4,10 @@ class TapController < UIViewController
 
     self.view.backgroundColor = UIColor.redColor
 
+    @label = UILabel.alloc.initWithFrame(CGRectZero)
+    @label.text = "Taps"
+    @label.sizeToFit
+    @label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2)
+    self.view.addSubview @label
   end
 end
