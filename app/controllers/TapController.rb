514c631247ae622b68005b54fa3adbb40487890b
yellowstar <845059331@qq.com>
2014-06-02 20:38:05 +0800

    add push method in TapController

--- app/controllers/TapController.rb
+++ app/controllers/TapController.rb
@@ -14,4 +14,9 @@ class TapController < UIViewController
     right_button = UIBarButtonItem.alloc.initWithTitle("Push", style: UIBarButtonItemStyleBordered, target:self, action:'push')
     self.navigationItem.rightBarButtonItem = right_button
   end
+
+  def push
+    new_controller = TapController.alloc.initWithNibName(nil, bundle: nil)
+    self.navigationController.pushViewController(new_controller, animated: true)
+  end
 end
